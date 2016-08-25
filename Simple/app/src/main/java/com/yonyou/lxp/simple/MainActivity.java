3596dd5602b35d01abaf803f34c94dfbca6c23a9
刘笑朋 <liuxpc@yonyou.com>
2016-08-25 13:01:45 +0800

    配置升级

--- Simple/app/src/main/java/com/yonyou/lxp/simple/MainActivity.java
+++ Simple/app/src/main/java/com/yonyou/lxp/simple/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yonyou.lxp.simple;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.orhanobut.logger.Logger;
 import com.yonyou.lxp.lxp_utils.adapter.CommonAdapter;
@@ -16,6 +18,7 @@ import com.yonyou.lxp.lxp_utils.base.BaseActivity;
 import com.yonyou.lxp.lxp_utils.net.Http;
 import com.yonyou.lxp.lxp_utils.net.ParamsMap;
 import com.yonyou.lxp.lxp_utils.net.ResponseMap;
+import com.yonyou.lxp.lxp_utils.utils.JsonUtils;
 import com.yonyou.lxp.simple.contract.MainContract;
 
 import java.lang.reflect.Type;
@@ -27,6 +30,9 @@ import java.util.Map;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends BaseActivity implements MainContract.IMianView {
 
@@ -89,12 +95,50 @@ public class MainActivity extends BaseActivity implements MainContract.IMianView
     @Override
     public void doBusiness() {
 
-//        ParamsMap paramsMap = new ParamsMap(JOKE_URL, JOKE_URL_RAND)
-//                .put("key", JUHE_JOKE_KEY);
-////        ResponseMap responseMap=new ResponseMap().putData()
+        ParamsMap paramsMap = new ParamsMap(JOKE_URL, JOKE_URL_RAND)
+                .put("key", JUHE_JOKE_KEY);
+//        ResponseMap responseMap=new ResponseMap().putData()
 //        Http.post(paramsMap, null, new Http.HttpCallBack() {
 //            @Override
 //            public void isSuccess(String data, Map<String, Object> mapData) {
+//                Observable.just(data).filter(new Func1<String, Boolean>() {
+//                    @Override
+//                    public Boolean call(String s) {
+//                        return JsonUtils.isSuccess(s, "code", "1");
+//                    }
+//                });
+//
+//
+//                final bean[] bs = new bean[]{};
+//                Observable.from(bs).map(new Func1<bean, String>() {
+//                    @Override
+//                    public String call(bean bean) {
+//                        return bean.name;
+//                    }
+//                }).subscribe(new Action1<String>() {
+//                    @Override
+//                    public void call(String s) {
+//                        System.out.println(s);
+//                    }
+//                });
+//
+//
+//                Observable.from(bs).subscribe(new Action1<bean>() {
+//                    @Override
+//                    public void call(bean bean) {
+//                        for (String ss : bean.list) {
+//                            System.out.println(ss);
+//                        }
+//                    }
+//                });
+//
+//
+//                Observable.from(bs).
+//                        flatMap(b -> Observable.from(b.list))
+//                        .subscribe(s -> System.out.println(s));
+//
+//
+//
 //
 //            }
 //
@@ -103,10 +147,65 @@ public class MainActivity extends BaseActivity implements MainContract.IMianView
 //
 //            }
 //        });
+
+
+        String jsonStrData = "{\"code\":\"1\",\"data\":{\"name\":\"阿三\",\"age\":\"18\"},\"count\":\"1\"}";
+
+        Observable.just(jsonStrData)
+                .filter(strData -> JsonUtils.isSuccess(strData, "code", "1"))
+                .map(s ->  getInfo(JsonUtils.getJsonStr(s, "data"), User.class))
+                .subscribe(u -> Logger.e(u.getName())
+                        , throwable -> Logger.e(throwable.getMessage()));
+
+
+
+        if (JsonUtils.isSuccess(jsonStrData, "code", "1")){
+            getInfo(JsonUtils.getJsonStr(jsonStrData, "data"), User.class);
+        }else {
+            showToast(mContext,"err");
+        }
+
+
     }
+
+
+    private <T>T getInfo(String jsonStr, Class<T> clazz) {
+        return gson.fromJson(jsonStr, clazz);
+    }
+
+    private Gson gson = new Gson();
+
 
     @Override
     public void refreshError(String error) {
 
+    }
+
+
+    class bean {
+        private String name;
+        private List<String> list;
+    }
+
+
+    class User {
+        private String name;
+        private String age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        public void setAge(String age) {
+            this.age = age;
+        }
     }
 }
